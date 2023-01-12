2301b8c36147d4e23d82ba2016268f726e127266
Theodore Lessman <theospatial@gmail.com>
2023-01-11 21:38:08 -0800

    added sats/USD display

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import axios from "axios";
 
 function App() {  
   const [price, setPrice] = useState(0);
+  const [moscow, setMoscow] = useState(0);
   //() => {};
   //callback function: call it and pass in another function
   const getPrice = () => {
@@ -19,15 +20,32 @@ function App() {
         console.log(err);
       });
   }
+  const getMoscow = () => {
+    //axios is way to create async
+    axios
+      .get("https://api.coinbase.com/v2/prices/BTC-USD/spot")
+      //.then is a promise that will run when the API call is successful
+      .then((res) => {
+        setMoscow(Math.floor(1/(res.data.data.amount*0.00000001)));
+      })
+      //.catch is a promise that will run if the API call fails
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
   useEffect(() => {
     getPrice();
+    getMoscow();
   }, []);
 
   return (
     <div className="App">
       <h1>Bitcoin Price</h1>
       <h3>${price}</h3>
+
+      <h1>Sats per Dollar</h1>
+      <h3>{moscow} sats</h3>
     </div>
   );
 }
